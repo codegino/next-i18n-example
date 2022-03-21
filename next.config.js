051da9184d4525e67c23b4aa6d9bf712dff3023a
codegino <carloginocatapang@gmail.com>
2022-03-21 22:16:11 +0800

    feat: Added built-in i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "sv", "cn"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
