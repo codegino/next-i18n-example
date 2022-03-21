b3e5b8c1dac3665eb10e1b3bff0616e90516c1ed
codegino <carloginocatapang@gmail.com>
2022-03-21 23:46:30 +0800

    feat: Added translation in about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,16 @@
 import { useRouter } from "next/router";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function About() {
   const { locale } = useRouter();
 
   return <main>About page: {locale}</main>;
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
