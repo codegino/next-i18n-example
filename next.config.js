2553055799766b60cde90c5332d51dd562c016aa
codegino <carloginocatapang@gmail.com>
2022-03-21 22:54:06 +0800

    chore: Added next-i18next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const { i18n } = require("./next-i18next.config");
+
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "sv", "cn"],
-  },
+  i18n,
 };
 
 module.exports = nextConfig;
